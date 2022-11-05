ee613417588453b0690e79c75d62f02e30e449d7
Komal suryawanshi <107236171+suryawanshi55@users.noreply.github.com>
2022-11-05 19:05:52 +0530

    Add files via upload

--- ALL PROJECT/index.js
+++ ALL PROJECT/index.js
@@ -1,33 +1,5 @@
-// Declaring html element
-
-const imgdiv=document.querySelector(" .profilepic")
-   
-
-const img=document.querySelector("#photo")
-const file=document.querySelector("#file")
-const uploadbtn=document.querySelector("#uploadbtn")
-
-// if user hover on profile div
-imgdiv.addEventListener('mousecenter',function(){
-    uploadbtn.style.display="block"
-})
-
-// if user hover out from img div
-imgdiv.addEventListener('mouseleave',function(){
-    uploadbtn.style.display="none"
-})
-
-// for chossing image to upload
-
-file.addEventListener('change',function(){
-    const choosefile=this.files[0];
-    if (choosefile){
-        const reader = new FileReader();
-        // filereder is pre defined function in js
-
-        reader.addEventListener('load',function(){
-            img.setAttribute('src',reader.result)
-        })
-        reader.readAsDataURL(choosefile);
-    }
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle("slow");
+    })
 })
